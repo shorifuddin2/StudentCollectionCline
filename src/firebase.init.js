baa896a98a3406c20f3ca2534cbf68fb0bfe24d9
shorifuddin2 <shorifuddin240915@gmail.com>
2023-03-29 03:10:59 +0600

    Arthontication code add

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCmM5uIvgZQi9n7-JPnRr1qzIb12fFp-Pc",
-  authDomain: "studentsystem-d9524.firebaseapp.com",
-  projectId: "studentsystem-d9524",
-  storageBucket: "studentsystem-d9524.appspot.com",
-  messagingSenderId: "966229480135",
-  appId: "1:966229480135:web:8330516cc3fcb876ce64e2",
-  measurementId: "G-0CGTNBD10H"
+  apiKey: "AIzaSyAfy960ddoJatRbMDehKpBNzzAChAMvcPU",
+  authDomain: "student-app-a5b1d.firebaseapp.com",
+  projectId: "student-app-a5b1d",
+  storageBucket: "student-app-a5b1d.appspot.com",
+  messagingSenderId: "58262830236",
+  appId: "1:58262830236:web:2bedfd853af17e59978159",
+  measurementId: "G-DLTHGS1V8H"
 };
 
 // Initialize Firebase
